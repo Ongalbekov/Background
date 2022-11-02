722d9358fe98d3141ba60cfc7445189df9b019ce
dauren <davron87@web.de>
2022-11-02 23:43:48 +0100

    #

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,4 @@
-changeBackground=(r)=>{
+changeBackground=()=>{
     let red=document.querySelector('#red').value;
     let green=document.querySelector('#green').value;
     let blue=document.querySelector('#blue').value;
